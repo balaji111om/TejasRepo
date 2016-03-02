c0562038fd64f276d3be06f6fda05b6f567aafcc
Balaji <balaji111om@gmail.com>
2016-03-02 15:54:22 -0600

    second change

--- secondpro/src/Pack2/NlsClass.java
+++ secondpro/src/Pack2/NlsClass.java
@@ -5,8 +5,8 @@ public class NlsClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("Hello NLS");
+		System.out.println("Hello guys");
+		System.out.println("Hello NLS 12333");
 	}
 
 }
