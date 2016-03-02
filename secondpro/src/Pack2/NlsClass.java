b479990d75a6da12bce4014211eed80fe4a9b204
balaji111om <balaji111om@gmail.com>
2016-03-02 15:55:47 -0600

    Update NlsClass.java
    change from git

--- secondpro/src/Pack2/NlsClass.java
+++ secondpro/src/Pack2/NlsClass.java
@@ -4,7 +4,7 @@ public class NlsClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Changwe from Git");
 		System.out.println("Hello guys");
 		System.out.println("Hello NLS 12333");
 	}
